6d8c434fdbfb5945fbc6ae4e06f1603a0011d142
tamiz-uddin <tamizuddin.tr@gmail.com>
2026-07-13 14:46:19 +0600

    VPS deployment updates

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 echo "Waiting for PostgreSQL..."
 
 while ! nc -z db 5432; do
@@ -8,11 +10,13 @@ done
 
 echo "Database Ready"
 
+mkdir -p logs
+
 python manage.py migrate
 
 python manage.py collectstatic --noinput
 
 exec gunicorn config.wsgi:application \
-      --bind 0.0.0.0:8000 \
-      --workers 4 \
-      --timeout 120
+    --bind 0.0.0.0:8000 \
+    --workers 4 \
+    --timeout 120
